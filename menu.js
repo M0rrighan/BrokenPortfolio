28d7f95341a40fcf0c1c65bcba7b58e11ac2d553
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-06-29 01:06:21 +0200

    Add evecnt handlers

--- menu.js
+++ menu.js
@@ -1,25 +1,38 @@
-var menu = document.querySelector(".menu");
-var nav = document.querySelector("nav");
-var menu_btn = document.querySelector(".menu_btn");
-var menu_icon = document.querySelector("nav a i");
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+const menuBtn = document.querySelector('.menu_btn');
+const menuIcon = document.querySelector('nav a i');
+const links = Array.from(menu.children);
+
+function hideMenu() {
+  nav.classList.remove('nav');
+  menu.style.display = 'none';
+  menuBtn.classList.replace('btn_flex_end', 'btn_flex_center');
+  menuIcon.classList.add('primary_b');
+  menuIcon.innerHTML = 'menu';
+}
+
+function showMenu() {
+  nav.classList.add('nav');
+  menu.style.display = 'flex';
+  menuBtn.classList.replace('btn_flex_center', 'btn_flex_end');
+  menuIcon.classList.remove('primary_b');
+  menuIcon.innerHTML = 'close';
+  menu.style.animation = 'none';
+}
 
 function toggleMenu() {
-  if (menu.style.display == "none") {
-    nav.classList.add("nav");
-    menu.style.display = "flex";
-    menu_btn.classList.replace("btn_flex_center", "btn_flex_end");
-    menu_icon.classList.remove("primary_b");
-    menu_icon.innerHTML = "close";
+  if (menu.style.display === 'none') {
+    showMenu();
   } else {
     hideMenu();
   }
-  menu.style.animation = "none";
 }
 
-function hideMenu() {
-  nav.classList.remove("nav");
-  menu_btn.classList.replace("btn_flex_end", "btn_flex_center");
-  menu_icon.classList.add("primary_b");
-  menu_icon.innerHTML = "menu";
-  menu.style.display = "none";
+menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener('touchstart', toggleMenu);
+
+for (let i = 0; i < links.length; i += 1) {
+  links[i].addEventListener('click', hideMenu);
+  links[i].addEventListener('touchstart', hideMenu);
 }
