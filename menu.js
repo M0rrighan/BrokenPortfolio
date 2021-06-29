04bff52605854870b3d23312d4ccbe7613fac642
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-06-29 03:34:34 +0200

    Add blur and prevent scroll of background page on mobile menu active, fix NavLinks font weight

--- menu.js
+++ menu.js
@@ -1,24 +1,39 @@
-const menu = document.querySelector('.menu');
+const body = document.getElementsByTagName('body')[0];
+const logo = document.querySelector('.logo_placeholder');
 const nav = document.querySelector('nav');
+const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.menu_btn');
 const menuIcon = document.querySelector('nav a i');
-const links = Array.from(menu.children);
+const navLinks = document.querySelectorAll('.menu li a');
+const main = document.getElementById('main');
 
 function hideMenu() {
+  body.style.overflow = 'auto';
+  main.style.filter = 'blur(0)';
+  logo.style.filter = 'blur(0)';
   nav.classList.remove('nav');
   menu.style.display = 'none';
   menuBtn.classList.replace('btn_flex_end', 'btn_flex_center');
   menuIcon.classList.add('primary_b');
   menuIcon.innerHTML = 'menu';
+  for (let i = 0; i < navLinks.length; i += 1) {
+    navLinks[i].classList.remove('fw_600');
+  }
 }
 
 function showMenu() {
+  body.style.overflow = 'hidden';
+  main.style.filter = 'blur(4px)';
+  logo.style.filter = 'blur(4px)';
   nav.classList.add('nav');
   menu.style.display = 'flex';
   menuBtn.classList.replace('btn_flex_center', 'btn_flex_end');
   menuIcon.classList.remove('primary_b');
   menuIcon.innerHTML = 'close';
   menu.style.animation = 'none';
+  for (let i = 0; i < navLinks.length; i += 1) {
+    navLinks[i].classList.add('fw_600');
+  }
 }
 
 function toggleMenu() {
@@ -32,7 +47,7 @@ function toggleMenu() {
 menuBtn.addEventListener('click', toggleMenu);
 menuBtn.addEventListener('touchstart', toggleMenu);
 
-for (let i = 0; i < links.length; i += 1) {
-  links[i].addEventListener('click', hideMenu);
-  links[i].addEventListener('touchstart', hideMenu);
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', hideMenu);
+  navLinks[i].addEventListener('touchstart', hideMenu);
 }
